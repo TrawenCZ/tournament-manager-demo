8813b8dd01f961a11c71fe762fb0eaffec91e55c
Adam Slíva <xsliva@fi.muni.cz>
2023-05-07 18:53:32 +0200

    Initial commit

---
+++ TournamentManager3000/Models/Match.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentManager3000.Models
+{
+    internal class Match
+    {
+        public int Id { get; set; }
+        public int TournamentId { get; set; }
+        public int Team1Id { get; set; }
+        public int Team2Id { get; set; }
+        public int TeamWinnerId { get; set; }
+    }
+}
---
+++ TournamentManager3000/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentManager3000.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+
+    }
+}
